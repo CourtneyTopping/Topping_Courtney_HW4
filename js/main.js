294e7d5f2c95f66b84e6b3b898dd3eca8ab37968
Topping <c_topping@tss-hr420-dm32.fc.ca>
2018-03-29 10:08:07 -0400

    js video

--- js/main.js
+++ js/main.js
@@ -1,27 +1,59 @@
 (() =>{
   console.log('video stuff loaded');
+
+  //custom function on object prototype
+  String.prototype.capIt = function() { return this.replace(this.charAt(),
+  this.charAt().toUpperCase()); }
+
+
   //variables come first
   let sigils = document.querySelectorAll('.sigilContainer'),
       lightbox = document.querySelector('.lightbox'),
-      closeLightBoxButton = lightbox.querySelector('.close-lightbox');
+      closeLightBoxButton = lightbox.querySelector('.close-lightbox'),
+      vidPlayer = document.querySelector('video'),
+      vidControls = document.querySelector('.controls');
 
 //Functions in the middle
 function showHouseVideo() {
+  let houseName = this.className.split(' ')[1].capIt();
+//split apart the class name on the element, grab the house from the result
+  document.querySelector('h1').textContent = `House ${houseName}`;
   //debugger;
   lightbox.classList.add('show-lightbox');
-  //make it play -->take autoplay off in index.html
+  //make it play -->(take autoplay off in index.html)
+  vidPlayer.src = `video/House-${houseName}.${vidPlayer.currentSrc.split('.')[1]}`;
+  vidPlayer.load();
+  vidPlayer.play();
 }
 
 function closeLightbox() {
   //debugger;
   lightbox.classList.remove('show-lightbox');
   //next stop video and rewind to 0
+  vidPlayer.pause();
+  vidPlayer.currentTime = 0;
+}
 
+function pausePlay() {
+  //debugger;
+let theButton = this.firstElementChild;
+
+if (vidPlayer.paused == true) {
+  //play the video
+  vidPlayer.play();
+  theButton.dataset.icon = "pause-circle";
+} else {
+  //flip the icon to "play"
+  vidPlayer.pause();
+  theButton.dataset.icon = "play-circle";
+  }
 }
 
 //Event Handling at the bottom
 sigils.forEach(sigil => sigil.addEventListener('click', showHouseVideo));
 closeLightBoxButton.addEventListener('click', closeLightbox);
+vidPlayer.addEventListener('ended', closeLightbox);
+vidControls.addEventListener('click', pausePlay);
 
 })();
 
